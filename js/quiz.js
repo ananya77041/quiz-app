36b0d0ce45a68207343e74d1cd3d6fb06651bd9e
Andy Mishra <andy.mishra@opower.com>
2015-07-10 13:13:13 -0400

    improved styles and completed functionality

--- js/quiz.js
+++ js/quiz.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	/* GLOBAL VARS */
 	var pulseProperties = {
-		backgroundColor: "grey"
+		backgroundColor: "#252E40"
 	};
 
 	// Tracks current question number
@@ -21,6 +21,9 @@ $(document).ready(function() {
 		$('#question').textfill({
 		});
 		for (i=1; i <= 4; i++) {
+			if ($('#answer' + i).hasClass('hidden')) {
+				$('#answer' + i).removeClass('hidden');
+			};
 			$('#answer' + i + " p").text(QUESTIONS[qNum-1].answers[i-1]);
 		};
 	};
@@ -29,44 +32,61 @@ $(document).ready(function() {
 	var markCorrect = function() {
 		$('#question span').text("Correct!");
 		$('#question').textfill();
-		$('#answer' + QUESTIONS[currentQuestion].correct).addClass('correct');
-		$('#node' + currentQuestion).pulse('destroy')
-		setTimeout(function() {
-			$('#node' + currentQuestion).css('background-color', 'green');
-			currentQuestion++;
-			$('#node' + currentQuestion).removeClass('unanswered')
-			.pulse(pulseProperties, {
-				pulses : -1,
-				duration: 2000
-			});
-			$('#answer' + QUESTIONS[currentQuestion].correct).removeClass('correct');
-			displayQuestion(currentQuestion);
-		}, 3000);
+		$('#answer' + QUESTIONS[currentQuestion-1].correct).addClass('correct');
+		$('#node' + currentQuestion).pulse('destroy');
 	};
 
 	// Incorrect answer
 	var markIncorrect = function(choice) {
 		$('#question span').text("Incorrect...");
 		$('#question').textfill();
-		$('#answer' + QUESTIONS[currentQuestion].correct).addClass('correct');
+		$('#answer' + QUESTIONS[currentQuestion-1].correct).addClass('correct');
 		$('#answer' + choice).addClass('incorrect');
-		$('#node' + currentQuestion).pulse('destroy')
-		setTimeout(function() {
-			$('#node' + currentQuestion).css('background-color', 'red');
+		$('#node' + currentQuestion).pulse('destroy');
+	};
+
+	// Prepare for next question
+	var nextQuestion = function(prevResult) {
+		if (prevResult === "correct") {
+			$('#node' + currentQuestion).removeAttr('style').addClass('correct');
+		}
+		else {
+			$('#node' + currentQuestion).removeAttr('style').addClass('incorrect');
+		};
+		if (currentQuestion == QUESTIONS.length) {
+			$('#quiz').fadeOut(500);
+			$('#score').fadeIn(500, function() {
+				displayScore();
+			});
+		}
+		else {
 			currentQuestion++;
 			$('#node' + currentQuestion).removeClass('unanswered')
 			.pulse(pulseProperties, {
 				pulses : -1,
-				duration: 2000
+				duration: 1000
 			});
-			$('.answer').removeClass('correct').removeClass('incorrect');
 			displayQuestion(currentQuestion);
-		}, 3000);
+		};
+		$('.answer').removeClass('correct').removeClass('incorrect');
+	};
+
+	// Display the score
+	var displayScore = function() {
+		var score = $('.node.correct').length;
+		if (score > 5) {
+			var msg = "Wow! You got " + score + " out of " + QUESTIONS.length + "! Nice Job!";
+		}
+		else {
+			var msg = "You only got " + score + " out of " + QUESTIONS.length;
+			$('#restart').fadeIn(500);
+		}
+		$('#score h1').text(msg);
 	};
 
 	/* BEGIN MAIN SCRIPT */
 
-	// Create questions
+	// Create mock questions
 	var QUESTIONS = []
 	for (i=0;i<10;i++) {
 		QUESTIONS[i] = new Question(
@@ -80,12 +100,15 @@ $(document).ready(function() {
 	};
 
 	// Start quiz
-	$('#start').on('click', function() {
-		$('#start-state').fadeOut(500);
-		$('#quiz').fadeIn(500);
+	$('#start, #restart').on('click', function() {
+		$('.node').removeClass('correct').removeClass('incorrect');
+		#('button').removeClass('disabled');
+		$('.overlay').fadeOut(500, function() {
+			$('#quiz').fadeIn(500);
+		});
 		$('#node1').removeClass('unanswered').pulse(pulseProperties, {
 			pulses : -1,
-			duration: 2000
+			duration: 1000
 		});
 		displayQuestion(1);
 	})
@@ -93,20 +116,38 @@ $(document).ready(function() {
 	// Answer a question
 	$('.answer').on('click', function() {
 		var choice = String(this.id).charAt(6);
-		if (choice == QUESTIONS[currentQuestion].correct) {
+		if (choice == QUESTIONS[currentQuestion-1].correct) {
 			markCorrect();
+			setTimeout(function() {
+				nextQuestion("correct");
+			}, 3000);
 		}
 		else {
 			markIncorrect(choice);
+			setTimeout(function() {
+				nextQuestion("incorrect");
+			}, 3000);
 		};
 	});
 
 	// Google it
-	$('#google').on('click', function() {
+	$('#google').one('click', function() {
+		$('#google').addClass('disabled');
 		window.open("http://www.google.com/?#q=" + $('#question span').text());
 	});
 
 	// 50:50
-	$('#5050').on('click', function() {
+	$('#5050').one('click', function() {
+		$('#5050').addClass('disabled');
+		var incorrect = [];
+		for (i=1;i<=4;i++) {
+			if (i != QUESTIONS[currentQuestion-1].correct) {
+				incorrect.push(i);
+			};
+		};
+		while ($('.answer.hidden').length < 2) {
+			var random = incorrect[Math.floor(Math.random()*3)];
+			$('#answer' + random).addClass('hidden').fadeOut(500);
+		};
 	});
 })
